5adf175648e767a15555f198d5109f944c13dafb
Hakizimana Fred <hakifred20@gmail.com>
2023-09-18 15:38:40 +0300

    basic wc tool implementation

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { readFileSync } from "fs";
+import path from "path";
 
 function main() {
   // get args
@@ -17,8 +18,25 @@ function main() {
 function reader(file) {
   try {
     const data = readFileSync(file, "utf-8");
-    console.log(data);
+    const lines = data.split("\n");
+    const numLines = lines.length;
+    const words = data.split(/\s+/).filter(Boolean);
+    const numWords = words.length;
+    const numChars = data.length;
+
+    const myFile = extractFileName(file); // check if file includes path
+    const output = `${numLines} ${numWords} ${numChars} ${myFile}`;
+
+    console.log(output);
   } catch (e) {}
+}
+
+function extractFileName(pathToFile) {
+  if (pathToFile.includes("/")) {
+    const fileName = path.basename(pathToFile);
+    return fileName;
+  }
+  return pathToFile;
 }
 
 main();
